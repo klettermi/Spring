4f05f8b1e7f601e763858962556253eccef21319
Mi <mi_aa_@naver.com>
2023-12-18 09:54:38 +0900

    chore: package 이동

--- src/main/java/mi/spring/config/WebSecurityConfig.java
+++ src/main/java/mi/spring/config/WebSecurityConfig.java
@@ -1,9 +1,15 @@
-package mi.spring.security.config;
+package mi.spring.config;
 
+import lombok.RequiredArgsConstructor;
+import mi.spring.security.UserDetailsServiceImpl;
+import mi.spring.security.jwt.JwtAuthenticationFilter;
+import mi.spring.security.jwt.JwtUtil;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,7 +18,23 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+    private final JwtUtil jwtUtil;
+    private UserDetailsServiceImpl userDetailsService;
+    private AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception{
+        JwtAuthenticationFilter filter = new JwtAuthenticationFilter(jwtUtil);
+        filter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
+        return filter;
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.csrf(AbstractHttpConfigurer::disable);
@@ -20,7 +42,8 @@ public class WebSecurityConfig {
         http.authorizeHttpRequests((authorizeHttpRequests) ->
                 authorizeHttpRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                        .anyRequest().authenticated());
+                        .anyRequest().authenticated())
+                        .addFilterBefore(jwtAuthenticationFilter(), JwtAuthenticationFilter.class);
 
         http.formLogin(Customizer.withDefaults());
         return http.build();
--- src/main/java/mi/spring/user/dto/LoginRequestDto.java
+++ src/main/java/mi/spring/user/dto/LoginRequestDto.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Setter
 public class LoginRequestDto {
     private String username;
     private String password;
