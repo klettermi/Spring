1380c3ff371d42af7b3367819f8c3752948c8021
Mi <mi_aa_@naver.com>
2023-12-18 09:33:22 +0900

    feat: UserDetailsServiceImpl

---
+++ src/main/java/mi/spring/security/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package mi.spring.security;
+
+import mi.spring.user.entity.User;
+import mi.spring.user.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("없는 사용자입니다."));
+        return new UserDetailsImpl(user);
+    }
+}
