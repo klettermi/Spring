3cd7e86d9f7313e21e4bd8703845273a2bcfc394
Mi <mi_aa_@naver.com>
2023-12-18 09:33:03 +0900

    feat: UserDetailsImpl

---
+++ src/main/java/mi/spring/security/UserDetailsImpl.java
@@ -0,0 +1,55 @@
+package mi.spring.security;
+
+import mi.spring.user.entity.User;
+import mi.spring.user.entity.UserRoleEnum;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails {
+    private final User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    public UserRoleEnum getRole(){
+        return user.getRole();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
